a0f77121b0ffa864ffa2720d0787a3550ec5aeff
hari <harikrushnvaghasiya777@gmail.com>
2025-01-22 17:42:57 +0530

    add data entry in notification

--- src/screen/PushNotification.tsx
+++ src/screen/PushNotification.tsx
@@ -1,8 +1,21 @@
 import notifee, {EventType} from '@notifee/react-native';
-import React, {useEffect} from 'react';
-import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {createTable, fetchData, insertData} from '../service/database';
+import {push_notification} from '../query/todo';
 
 const PushNotification = () => {
+  const [notificationResponse, setNotificationResponse] = useState<any[]>([]);
+  console.log('notificationResponse: ', notificationResponse);
+
   // Request permission for notifications (needed for iOS)
   const requestPermission = async () => {
     const settings = await notifee.requestPermission();
@@ -52,6 +65,20 @@ const PushNotification = () => {
     });
   };
 
+  const loadNotifiactionResponse = async (): Promise<void> => {
+    const notificationResponseFromDb = await fetchData(
+      push_notification.fetchResponseNotification,
+    );
+    setNotificationResponse(notificationResponseFromDb);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await createTable(push_notification.crateTbl);
+      await loadNotifiactionResponse();
+    })();
+  }, []);
+
   // Set up background notification listener (optional)
   useEffect(() => {
     const unsubscribe = notifee.onForegroundEvent(async ({type, detail}) => {
@@ -60,9 +87,9 @@ const PushNotification = () => {
       if (type === EventType.ACTION_PRESS) {
         const {pressAction} = detail;
         if (pressAction && pressAction.id === 'yes-action') {
-          console.log('User pressed Yes');
+          await insertData(push_notification.inserDara, [1, new Date()]);
         } else if (pressAction && pressAction.id === 'no-action') {
-          console.log('User pressed No');
+          await insertData(push_notification.inserDara, [0, new Date()]);
         }
       }
     });
@@ -73,11 +100,34 @@ const PushNotification = () => {
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Button title="Display Notification" onPress={onDisplayNotification} />
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Button
+            title="Display Notification"
+            onPress={onDisplayNotification}
+          />
+        </View>
+      </SafeAreaView>
+      <FlatList
+        data={notificationResponse}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={styles.taskContainer}>
+            <View style={styles.taskDetails}>
+              <Text
+                style={[
+                  styles.taskTitle,
+                  item.completed ? styles.taskCompleted : null,
+                ]}>
+                {item.notification_time}
+              </Text>
+              <Text style={styles.taskDescription}>{item.response_status}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </>
   );
 };
 
@@ -86,7 +136,19 @@ export default PushNotification;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
+  taskContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    padding: 12,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+  },
+  taskDetails: {flex: 1, marginLeft: 8},
+  taskTitle: {fontSize: 16, fontWeight: 'bold'},
+  taskDescription: {fontSize: 14, color: '#555'},
+  taskCompleted: {textDecorationLine: 'line-through', color: 'gray'},
 });
