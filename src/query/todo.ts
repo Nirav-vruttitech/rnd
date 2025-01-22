70d5f50dcd262437ff0e5f677615032f82c62975
hari <harikrushnvaghasiya777@gmail.com>
2025-01-22 17:42:12 +0530

    add query

--- src/query/todo.ts
+++ src/query/todo.ts
@@ -1,4 +1,15 @@
 export const todo_table = {
   createTbl:
     'CREATE TABLE IF NOT EXISTS Tasks (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,description TEXT,completed INTEGER DEFAULT 0);',
+  fetchData: `SELECT * FROM Tasks;`,
+};
+
+export const push_notification = {
+  crateTbl: `CREATE TABLE IF NOT EXISTS notification (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    response_status INTEGER NOT NULL CHECK(response_status IN (0, 1)),
+    notification_time TEXT NOT NULL
+    );`,
+  fetchResponseNotification: `SELECT * FROM notification`,
+  inserDara: `Insert into notification (response_status, notification_time) values (?,?);`,
 };
